ac658198134024cfbb5f1a936c48621e763fa5f3
poonam_b <poonam_b@techjoomla.com>
2018-06-27 14:37:41 +0530

    Task #129795 chore: add com_subusers component in greenhouse

---
+++ administrator/helpers/roleAcess.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @version    SVN: <svn_id>
+ * @package    Techjoomla.Libraries
+ * @author     Techjoomla <extensions@techjoomla.com>
+ * @copyright  Copyright (c) 2009-2015 TechJoomla. All rights reserved.
+ * @license    GNU General Public License version 2 or later.
+ */
+
+// No direct access.
+defined('_JEXEC') or die();
+
+/**
+ * Helper class for common functions in JSocial library
+ *
+ * @package  Techjoomla.Libraries
+ * @since    1.0.4
+ */
+class JRoleAccess
+{
+	/**
+	 * Check
+	 *
+	 * @param   string   $link          link
+	 * @param   integer  $skipIfNoMenu  Decide to use Itemid from $input
+	 *
+	 * @return  item id
+	 *
+	 * @since  3.0
+	 */
+	public function check($user_id, $action_name, $action_client, $content_id = 0)
+	{
+		//die("jay here");
+	}
+
+}
